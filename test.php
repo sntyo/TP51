359f1dfcfe063959350e983e14900e156e62c6dc
SNTYO <625507997@qq.com>
2018-10-29 10:25:47 +0800

    first commit

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return 'hello, thinkphp';
+    }
+}
+
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return 'index';
+    }
+    public function hello($name)
+    {
+        return 'Hello'.$name;
+    }
+}
+
+
+
+
+
+
+
+
+?>
