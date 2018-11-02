48697ea442e0bd74f415397a58cc796c4e7b7b63
SNTYO <625507997@qq.com>
2018-11-02 17:11:04 +0800

    datebase config start

--- test.php
+++ test.php
@@ -1,33 +1,4 @@
 <?php
-namespace app\index\controller;
-
-class Index
-{
-    public function index()
-    {
-        return 'hello, thinkphp';
-    }
-}
-
-namespace app\index\controller;
-
-class Index
-{
-    public function index()
-    {
-        return 'index';
-    }
-    public function hello($name)
-    {
-        return 'Hello'.$name;
-    }
-}
-
-echo "test github remote ";
-
-
-
-
 
 
 
